255402913e64119a70525805c59edce46d1f3823
Mega Soegiarto <soegiarto.mega@gmail.com>
2019-11-11 15:47:34 -0800

    fix env and config for deployment

--- client/src/config.js
+++ client/src/config.js
@@ -3,6 +3,7 @@ export default {
   auth0: {
     clientId: process.env.REACT_APP_AUTH0_CLIENTID,
     domain: process.env.REACT_APP_AUTH0_DOMAIN,
-    audience: process.env.REACT_APP_AUTH0_AUDIENCE
+    audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+    redirect_uri: REACT_APP_AUTH0_REDIRECT_URI
   },
 };
